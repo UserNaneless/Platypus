24f8f9e413f90cd28cf4fe3bbbb714e6f7277763
Naneless <vgripor@gmail.com>
2025-09-09 23:05:33 +0300

    init

---
+++ Build/CMakeFiles/Platypus.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "Platypus.map"
+  "CMakeFiles/Platypus.dir/Core/Src/main.c.obj"
+  "CMakeFiles/Platypus.dir/Core/Src/main.c.obj.d"
+  "CMakeFiles/Platypus.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "CMakeFiles/Platypus.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "CMakeFiles/Platypus.dir/Core/Src/stm32f4xx_it.c.obj"
+  "CMakeFiles/Platypus.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "CMakeFiles/Platypus.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/Platypus.dir/Core/Src/syscalls.c.obj.d"
+  "CMakeFiles/Platypus.dir/Core/Src/sysmem.c.obj"
+  "CMakeFiles/Platypus.dir/Core/Src/sysmem.c.obj.d"
+  "CMakeFiles/Platypus.dir/startup_stm32f405xx.s.obj"
+  "Platypus.elf"
+  "Platypus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/Platypus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Build/CMakeFiles/Platypus.dir/startup_stm32f405xx.s.d
@@ -0,0 +1,2 @@
+CMakeFiles/Platypus.dir/startup_stm32f405xx.s.obj: \
+ /home/naneless/Documents/EngPrac/Platypus/startup_stm32f405xx.s
