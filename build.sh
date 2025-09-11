4d9e456bed7d72bde6e372d4e0d1618e2e693aa0
NaneFull <vgripor@gmail.com>
2025-09-11 15:02:25 +0300

    build_better

--- build.sh
+++ build.sh
@@ -3,13 +3,40 @@
 if [ -d "Build" ]; then
     rm -r Build
 fi
-mkdir Build
-cd Build
-if command -v cmake-stm >/dev/null 2>&1; then
-    echo "cmake-stm found"
-    cmake-stm ..
-else
-    echo "cmake-stm not found, using cmake"
-    cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/gcc-arm-none-eabi.cmake 
+
+BUILD_TYPE=Debug
+BUILD=1
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --Release)
+            BUILD_TYPE=Release;
+            shift
+            ;;
+        --Debug)
+            BUILD_TYPE=Debug;
+            shift
+            ;;
+        --switch)
+            BUILD=0;
+            shift
+            ;;
+        *)
+            echo "Unknown parameter passed: $1"
+            exit 1
+            ;;
+    esac
+done
+
+BUILD_DIR=Build/$BUILD_TYPE
+
+mkdir -p $BUILD_DIR
+
+cmake -S . -B $BUILD_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_TOOLCHAIN_FILE=./cmake/gcc-arm-none-eabi.cmake 
+ln -sf $BUILD_DIR/compile_commands.json compile_commands.json
+ln -sf $BUILD_DIR/Platypus.elf Platypus.elf
+
+if [ $BUILD -eq 1 ]; then
+    make -C $BUILD_DIR
 fi
-make
+
